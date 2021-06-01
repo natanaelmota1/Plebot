34cf6205f4f8b5ff098a0c33c98e2de0ba219371
pauloacf <pauloacf148@gmail.com>
2021-05-31 23:12:05 -0400

    Update

--- plebot.py
+++ plebot.py
@@ -1,51 +1,47 @@
 import re
-import os
-from flask import Flask
+import unicodedata
+from webbot import *
 from interacoes import *
-import web
 
+'''
+MÉTODOS DO PLEBOT                                  
+'''
+# verifica qual é a mensagem do usuário e se há resposta do plebot
+def verifyMessage(message, question, response):
+    verify = re.search(question, message, re.IGNORECASE)
+    if (verify):
+        print("PLEBOT:"+ response)
+        return(True)
 
-app = Flask(__name__)
+# controla o fluxo de mensagens do plebot        
+def plebotMessage(message):
 
+    # removendo caracteres especiais
+    process = unicodedata.normalize("NFD", message)
+    process = process.encode("ascii", "ignore")
+    process = process.decode("utf-8")
+    
+    check = 'false'
+    for i in range(len(listResponses)):
+        if(verifyMessage(process, listResponses[i][0], listResponses[i][1])):
+            check = 'true'
+            plebotExit(len(listResponses), i)
+            break
+    
+    if (check == 'false'):
+        print("Não entendo tais palavras, é melhor que você não esteja fazendo "
+            + "bruxaria.")
 
-@app.route('/')
-def Main():
-    '''
-    MÉTODOS DO PLEBOT                                  
-    '''
-    # verifica qual é a mensagem do usuário e se há resposta do plebot
-    def verifyMessage(message, question, response):
-        verify = re.search(question, message, re.IGNORECASE)
-        if (verify):
-            return "<h1>"+response+"</h1>"
+# controla quando a conversação deve terminar            
+def plebotExit(listLen, listPos):
+    if( listPos == listLen - 1 or listPos == listLen - 2 or 
+        listPos == listLen - 3 or listPos == listLen - 4 ):
+        exit(1)
 
-    # controla o fluxo de mensagens do plebot
-    def plebotMessage(message):
-        check = 'false'
-        for i in range(len(listResponses)):
-            if(verifyMessage(message, listResponses[i][0], listResponses[i][1])):
-                check = 'true'
-                plebotExit(len(listResponses), i)
-                break
+'''
+FLUXO DO PLEBOT
+'''
 
-        if (check == 'false'):
-            return "<h1>Não entendo tais palavras, é melhor que você não esteja fazendo bruxaria.</h1>"
-
-    # controla quando a conversação deve terminar
-    def plebotExit(listLen, listPos):
-        if(listPos == listLen - 1 or listPos == listLen - 2 or
-                listPos == listLen - 3 or listPos == listLen - 4):
-            exit(1)
-
-    '''
-    FLUXO DO PLEBOT
-    '''
-
-    while(True):
-        message = web.input()
-        plebotMessage(message)
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+while(True):
+    message = input("\n>>>     ")
+    plebotMessage(message)
